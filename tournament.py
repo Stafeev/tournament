3caf4e90b9ba5bd3b5dff5f687514bb3a96237da
Stafeev <dunlop300g@yandex.ru>
2015-07-24 00:58:50 -0700

    Tournament+readme

--- tournament.py
+++ tournament.py
@@ -9,7 +9,7 @@ import bleach
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=vagrant")
+    return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
